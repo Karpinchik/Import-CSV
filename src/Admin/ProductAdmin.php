09392c4ebb901d13b75700ea1a46d5872562284c
Karpinchik Sergey <s.karpinchik@itransition.com>
2021-05-18 15:03:52 +0300

    refactoring ProductAdmin

--- src/Admin/ProductAdmin.php
+++ src/Admin/ProductAdmin.php
@@ -8,17 +8,22 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ProductAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper): void
     {
+        $data = new \DateTime;
         $formMapper
             ->add('productName',  TextType::class)
             ->add('productDesc',  TextType::class)
             ->add('productCode',  TextType::class)
-            ->add('stock',  TextType::class)
-            ->add('cost',  TextType::class)
+            ->add('added',null, array('data' => $data))
+            ->add('discontinued')
+            ->add('stock',  IntegerType::class)
+            ->add('cost',  MoneyType::class, array('currency' => 'RUB'))
         ;
     }
 
