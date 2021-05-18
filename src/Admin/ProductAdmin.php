f8a220634c4fe4e1ab836779c9f1b0198fe9106c
Karpinchik Sergey <s.karpinchik@itransition.com>
2021-05-18 18:34:35 +0300

    Refactirint

--- src/Admin/ProductAdmin.php
+++ src/Admin/ProductAdmin.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Admin;
 
+use App\Entity\Product;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
@@ -13,17 +14,30 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ProductAdmin extends AbstractAdmin
 {
+    public function toString(object $object): string
+    {
+        return $object instanceof Product
+            ? $object->getProductName()
+            : 'Product';
+    }
+
     protected function configureFormFields(FormMapper $formMapper): void
     {
         $data = new \DateTime;
         $formMapper
-            ->add('productName',  TextType::class)
-            ->add('productDesc',  TextType::class)
-            ->add('productCode',  TextType::class)
-            ->add('added',null, array('data' => $data))
-            ->add('discontinued')
-            ->add('stock',  IntegerType::class)
-            ->add('cost',  MoneyType::class, array('currency' => 'RUB'))
+            ->with('Product inform')
+                ->add('productName',  TextType::class, array('help' => 'Enter product name'))
+                ->add('productDesc',  TextType::class, array('help' => 'Enter product description'))
+                ->add('productCode',  TextType::class, array('help' => 'Enter product code'))
+            ->end()
+            ->with('Product data')
+                ->add('added',null, array('data' => $data))
+                ->add('discontinued', null, array('help' => 'Enter data if product discontinued'))
+            ->end()
+            ->with('Product characteristics')
+                ->add('stock',  IntegerType::class, array('help' => 'Enter stock'))
+                ->add('cost',  MoneyType::class, array('currency' => 'RUB', 'help' => 'Enter cost product'))
+            ->end()
         ;
     }
 
