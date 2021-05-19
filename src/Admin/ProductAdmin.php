16815b8e1cacee2dd7d1c59b863f4a585f5fa54f
Karpinchik Sergey <s.karpinchik@itransition.com>
2021-05-19 15:28:37 +0300

    Reafctoring ProductAdmin

--- src/Admin/ProductAdmin.php
+++ src/Admin/ProductAdmin.php
@@ -8,12 +8,21 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
+/**
+ * Class ProductAdmin
+ * @package App\Admin
+ */
 class ProductAdmin extends AbstractAdmin
 {
+    /**
+     * @param object $object
+     * @return string
+     */
     public function toString(object $object): string
     {
         return $object instanceof Product
@@ -21,21 +30,33 @@ class ProductAdmin extends AbstractAdmin
             : 'Product';
     }
 
+    /**
+     * @param FormMapper $formMapper
+     */
     protected function configureFormFields(FormMapper $formMapper): void
     {
         $data = new \DateTime;
         $formMapper
-            ->add('productName',  TextType::class, array('help' => 'Enter product name'))
-            ->add('productDesc',  TextType::class, array('help' => 'Enter product description'))
-            ->add('productCode',  TextType::class, array('help' => 'Enter product code'))
-            ->add('added',null, array('data' => $data))
-            ->add('discontinued', null, array('help' => 'Enter data if product discontinued'))
-            ->add('stock',  IntegerType::class, array('help' => 'Enter stock. This value should be not less than 10'))
-            ->add('cost',  MoneyType::class, array('currency' => 'RUB', 'help' => 'Enter cost product. 
-            This value should be greater than 5 and less then 1000'))
+            ->with('Product inform', [ 'class' => 'col-md-8'])
+                ->add('productName',  TextType::class, array('help' => 'Enter product name'))
+                ->add('productCode',  TextType::class, array('help' => 'Enter product code'))
+                ->add('productDesc',  TextareaType::class, array('help' => 'Enter product description'))
+            ->end()
+            ->with('Product data', [ 'class' => 'col-md-8'])
+                ->add('added', null, array('data' => $data))
+                ->add('discontinued', null, array('help' => 'Enter data if product discontinued'))
+
+            ->end()
+            ->with('Product characteristics', [ 'class' => 'col-md-8'])
+                ->add('stock',  IntegerType::class, array('help' => 'Enter stock'))
+                ->add('cost',  MoneyType::class, array('currency' => 'RUB', 'help' => 'Enter cost product'))
+            ->end()
         ;
     }
 
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
     protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
     {
         $datagridMapper
@@ -47,6 +68,9 @@ class ProductAdmin extends AbstractAdmin
         ;
     }
 
+    /**
+     * @param ListMapper $listMapper
+     */
     protected function configureListFields(ListMapper $listMapper): void
     {
         $listMapper
